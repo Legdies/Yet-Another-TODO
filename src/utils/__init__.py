4051a767e5196fa7c10e78886dd1e606010d27f6
legdi <legdies@outlook.com>
2025-05-23 00:32:10 +0100

    Moved general_check() from expansions to .requiremen_check for future scalability

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,7 @@
 from storage.filemanager import FileManager
 from .models import TaskModel, FileManagerModel
-from .expansion import general_check, clear
+from .expansion import clear
+from .requirement_check import *
 
 
 __all__ = ['FileManagerModel', 'TaskModel', 'FileManager', 'general_check', 'clear']
